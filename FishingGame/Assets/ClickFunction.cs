70c29d797c5166f5c8e80dd25b056bab73c13240
niallsher <niall.sheridan@tufts.edu>
2022-10-10 20:19:09 -0400

    When click on rod in store, prints how much money you have

--- FishingGame/Assets/ClickFunction.cs
+++ FishingGame/Assets/ClickFunction.cs
@@ -7,6 +7,11 @@ public class ClickFunction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+      if(!PlayerPrefs.HasKey("money"))
+        {
+            PlayerPrefs.SetFloat("money", 0);
+        }
+
 
     }
 
@@ -16,9 +21,14 @@ public class ClickFunction : MonoBehaviour
 
     }
 
-    void OnMouseDown()
+    public void Load()
+    {
+      Debug.Log("Player has $" + PlayerPrefs.GetFloat("money"));
+    }
+    public void OnMouseDown()
     {
       Debug.Log("clicked");
+      Load();
     }
 }
 
