e1dd839ef186a2cab78468e2c5104dddea308163
samg-14 <sam.glaser@tufts.edu>
2022-10-24 17:28:12 -0400

    editing gameWon

--- FishingGame/Assets/scripts/gameWon.cs
+++ FishingGame/Assets/scripts/gameWon.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class gameWon : MonoBehaviour
 {
+    public GameObject commonText;
+    private int commonCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("game won");
+        getCommon();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void getCommon()
     {
-        
+        Debug.Log("start of getCommon");
+        commonCount = (int) PlayerPrefs.GetFloat("Fish_com");
+        Debug.Log(commonCount);
+        Text common = commonText.GetComponent<Text>();
+        common.text = "" + commonCount;
+        Debug.Log("Common count: ");
+        Debug.Log(common.text);
     }
 
     public void PlayAgain() {
