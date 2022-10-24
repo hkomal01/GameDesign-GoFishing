73f5b89cc04fb750420eaa543664e451c7597e25
samg-14 <sam.glaser@tufts.edu>
2022-10-24 18:52:23 -0400

    more end scene stuff.

--- FishingGame/Assets/scripts/gameWon.cs
+++ FishingGame/Assets/scripts/gameWon.cs
@@ -9,22 +9,33 @@ public class gameWon : MonoBehaviour
 {
     public GameObject commonText;
     private int commonCount;
+    public GameObject uncommonText;
+    private int uncommonCount;
+    public GameObject rareText;
+    private int rareCount;
+    public GameObject legendaryText;
+    private int legendaryCount;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("game won");
-        getCommon();
+        getCounts();
     }
 
-    public void getCommon()
+    public void getCounts()
     {
-        Debug.Log("start of getCommon");
         commonCount = (int) PlayerPrefs.GetFloat("Fish_com");
-        Debug.Log(commonCount);
         Text common = commonText.GetComponent<Text>();
-        common.text = "" + commonCount;
-        Debug.Log("Common count: ");
-        Debug.Log(common.text);
+        common.text += commonCount;
+        uncommonCount = (int)PlayerPrefs.GetFloat("Fish");
+        Text uncommon = uncommonText.GetComponent<Text>();
+        uncommon.text += uncommonCount;
+
+        rareCount = (int)PlayerPrefs.GetFloat("Fish_3");
+        Text rare = rareText.GetComponent<Text>();
+        rare.text += rareCount;
+        legendaryCount = (int)PlayerPrefs.GetFloat("Fish_5");
+        Text legendary = legendaryText.GetComponent<Text>();
+        legendary.text += legendaryCount;
     }
 
     public void PlayAgain() {
